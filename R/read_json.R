f1ff7b487656a7b9584f61ee80f1d04f6943e2d1
molelinux <meelis.utt@gmail.com>
2020-12-24 01:05:43 +0200

    read.json template to dev branch

---
+++ R/read_json.R
@@ -0,0 +1,8 @@
+#' Read json in as list
+#'
+#' Read in a json file and parse it into a R list.
+#' @param file the name of the file, where the json is read from.
+#' @export
+read.json <- function(file = ''){
+  # TODO: this function
+}
