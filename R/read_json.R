9ae27c7db944b044b226bc217d6eaab41a20d667
molelinux <meelis.utt@gmail.com>
2020-12-29 21:26:21 +0200

    Init implementation of read.json; there are some todos in the file

--- R/read_json.R
+++ R/read_json.R
@@ -5,4 +5,20 @@
 #' @export
 read.json <- function(file = ''){
   # TODO: this function
+  fileContent <- readChar(file,file.info(file)$size)
+
+  fileContent <- gsub(pattern = '{',replacement = 'list(',x = fileContent,fixed = T)
+  fileContent <- gsub(pattern = '[',replacement = 'c(',x = fileContent,fixed = T)
+  fileContent <- gsub(pattern = ':',replacement = '=',x = fileContent,fixed = T)
+  fileContent <- gsub(pattern = ']|}',replacement = ')',x = fileContent)
+  fileContent <- gsub(pattern = '\n|\t|\ \ ',replacement = ' ',x = fileContent)
+  fileContent <- gsub(pattern = '[f,F]alse',replacement = 'FALSE',x = fileContent)
+  fileContent <- gsub(pattern = '[t,T]rue',replacement = 'TRUE',x = fileContent)
+  # print(fileContent)
+  fileContent <- eval(parse(text = fileContent))
+  print(fileContent)
 }
+
+# TODO: -----
+# * if key is a number and unquoted, then the function gives an error.
+# * if last element ends with comma, it gives error (because no element)
