b7958b34fd03aa06a71b4b1b4e956f73bd0b4cdd
molelinux <meelis.utt@gmail.com>
2020-12-30 16:28:29 +0200

    trycatch for read.json, #3

--- R/read_json.R
+++ R/read_json.R
@@ -4,21 +4,18 @@
 #' @param file the name of the file, where the json is read from.
 #' @export
 read.json <- function(file = ''){
-  # TODO: this function
-  fileContent <- readChar(file,file.info(file)$size)
-
-  fileContent <- gsub(pattern = '{',replacement = 'list(',x = fileContent,fixed = T)
-  fileContent <- gsub(pattern = '[',replacement = 'c(',x = fileContent,fixed = T)
-  fileContent <- gsub(pattern = ':',replacement = '=',x = fileContent,fixed = T)
-  fileContent <- gsub(pattern = ']|}',replacement = ')',x = fileContent)
-  fileContent <- gsub(pattern = '\n|\t|\ \ ',replacement = ' ',x = fileContent)
-  fileContent <- gsub(pattern = '[f,F]alse',replacement = 'FALSE',x = fileContent)
-  fileContent <- gsub(pattern = '[t,T]rue',replacement = 'TRUE',x = fileContent)
-  # print(fileContent)
-  fileContent <- eval(parse(text = fileContent))
-  print(fileContent)
+  tryCatch({
+    file_content <- readChar(file,file.info(file)$size)
+    file_content <- gsub(pattern = '{',replacement = 'list(',x = file_content,fixed = T)
+    file_content <- gsub(pattern = '[',replacement = 'c(',x = file_content,fixed = T)
+    file_content <- gsub(pattern = ':',replacement = '=',x = file_content,fixed = T)
+    file_content <- gsub(pattern = ']|}',replacement = ')',x = file_content)
+    file_content <- gsub(pattern = '[f,F]alse',replacement = 'FALSE',x = file_content)
+    file_content <- gsub(pattern = '[t,T]rue',replacement = 'TRUE',x = file_content)
+    file_content <- eval(parse(text = file_content))
+    return(file_content)
+  },error = function(err){
+    print(err)
+    print('Given json file has incorrect format! Check that all keys are quoted and no empty values exist in json.')
+  })
 }
-
-# TODO: -----
-# * if key is a number and unquoted, then the function gives an error.
-# * if last element ends with comma, it gives error (because no element)
